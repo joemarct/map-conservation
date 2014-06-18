2936a83a17c0a31bdb437ad94cb92e75719c74f3
Joemar Taganna <joemar.ct@gmail.com>
2014-06-18 15:44:47 +0200

    Used zipped module link instead of git clone link

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(name='conservation_mapping',
       url='http://www.joemartaganna.com',
       scripts=['map_conservation.py'],
       data_files=[('src/map-conservation', ['conservation_mapping_script_base.txt'])],
+      setup_requires=['score-conservation'],
       dependency_links=[
-          'https://github.com/joemarct/score-conservation.git'
-      ],
-      setup_requires=['score-conservation']
+          'https://github.com/joemarct/map-conservation/archive/master.zip#egg=score-conservation-1.0'
+      ]
      )
